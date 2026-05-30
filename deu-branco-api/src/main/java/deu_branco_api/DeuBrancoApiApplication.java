5d2b28296bdce9cfa72753a2cff931d350a96704
JoaoReiiis <jzreiis@proton.me>
2026-05-30 18:26:13 -0300

    projeto inicial gerado pelo Spring Initializr

---
+++ deu-branco-api/src/main/java/deu_branco_api/DeuBrancoApiApplication.java
@@ -0,0 +1,13 @@
+package deu_branco_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeuBrancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeuBrancoApiApplication.class, args);
+	}
+
+}
---
+++ deu-branco-api/src/test/java/deu_branco_api/DeuBrancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package deu_branco_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeuBrancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
